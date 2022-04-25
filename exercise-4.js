942163b2088d4bea23a0098fc72df780d495f52d
danielfelipeq <danielquispe.m@gmail.com>
2022-04-25 12:51:34 -0500

    exercise 4 done

---
+++ exercise-4.js
@@ -0,0 +1,19 @@
+/* 
+Ejercicio 4
+Escribir una función llamada generarCodigo que
+reciba un string y retorne un nuevo string con
+las dos primeras letras de cada palabra. 
+*/
+const generarCodigo = str =>{
+    let code = ""
+    let newStr = str.toLowerCase().split(" ").map(x=>{
+        for(let i = 0; i < 2;i++){
+            code += x.charAt(i)
+        }
+    })
+    return code  
+}
+
+console.log(generarCodigo("hola mundo")) // "homu"
+console.log(generarCodigo("juan david berbeo")) // "judabe"
+console.log(generarCodigo("")) // ""
